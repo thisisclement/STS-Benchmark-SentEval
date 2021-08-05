5aaff34c7c4b8d9f5058c46f4af89b3ea6e94442
Clement <clem0007@e.ntu.edu.sg>
2021-08-05 21:00:00 +0800

    add args options for script

--- scripts/run_stsbenchmark.py
+++ scripts/run_stsbenchmark.py
@@ -1,14 +1,35 @@
-from similarityevaluation import EmbeddingSimilarityEval_STSB, SimilarityFunction
+#TODO: Test script if working
+
+import sys
+sys.path.insert(0, '..')
+
+from SimilarityEvaluation.similarityevaluation import EmbeddingSimilarityEval_STSB, SimilarityFunction
 import os
 
-os.environ['REQUESTS_CA_BUNDLE'] = '/etc/ssl/certs/ca.xtraman.org.pem' # to solve the SSL cert issue
+import argparse
 
-eval_data_path = 'data/sts-test-ml.csv'
+parser = argparse.ArgumentParser(description='Running STS-B benchmark')
 
-model_local_path = os.getenv("MODEL_LOCAL_PATH")
+parser.add_argument("-d", "--data_path", help="The data path for benchmarking.")
+parser.add_argument("-m", "--model_path", help="The model path for benchmarking.")
+parser.add_argument("-n", "--benchmark_name", help="Name of Benchmark run.")
+parser.add_argument("-o", "--output", help="Output path of the for Benchmark results.")
+parser.add_argument("-ssl", help="SSL Cert path (optional)", action="store_true")
 
-model_list = [model_local_path + "paraphrase-multilingual-mpnet-base-v2", model_local_path + "paraphrase-multilingual-MiniLM-L12-v2"]
+args = parser.parse_args()
 
+if args.ssl:
+    os.environ['REQUESTS_CA_BUNDLE'] = '/etc/ssl/certs/ca.xtraman.org.pem' # to solve the SSL cert issue
+
+eval_data_path = args.data_path
+
+if args.model_path:
+    model_local_path = args.model_path
+    model_list = [args.model_path + "/paraphrase-multilingual-mpnet-base-v2", args.model_path + "/paraphrase-multilingual-MiniLM-L12-v2"]
+else:
+    model_list = ["paraphrase-multilingual-mpnet-base-v2", "paraphrase-multilingual-MiniLM-L12-v2"]
+
+# running for each model
 for model in model_list:
-    sts_eval = EmbeddingSimilarityEval_STSB(model, eval_data_path, main_similarity=SimilarityFunction.COSINE, name="malay-stsb", show_progress_bar=True, write_csv=True)
-    sts_eval.run_eval(output_path='./benchmark_results/')
+    sts_eval = EmbeddingSimilarityEval_STSB(model, eval_data_path, main_similarity=SimilarityFunction.COSINE, name=args.benchmark_name, show_progress_bar=True, write_csv=True)
+    sts_eval.run_eval(output_path=args.output)
